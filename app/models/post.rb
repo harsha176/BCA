bc8d9efd4f941e9e395af1178d8ece8866960fd5
Harshavardhan Malipatel <hmalipa@ncsu.edu>
2011-10-02 14:26:36 -0400

    validations added for posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
   has_many :replies
   has_and_belongs_to_many :users
+
+  validates_presence_of :title
+  validates_presence_of :message
+  validates_presence_of :user_id
 end
