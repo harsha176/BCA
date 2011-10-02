2171788e9399f44229415c3894357c46ffd066bd
Harshavardhan Malipatel <hmalipa@ncsu.edu>
2011-10-02 14:27:08 -0400

    validations added for reply

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,6 +1,9 @@
 class Reply < ActiveRecord::Base
   belongs_to :post
-  validates_presence_of :reply
+
   belongs_to :replies_users
   has_and_belongs_to_many :users
+
+  validates_presence_of :reply
+  validates_presence_of :post_id
 end
